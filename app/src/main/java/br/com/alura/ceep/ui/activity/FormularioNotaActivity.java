1c415ff760a4f91b03661b3a3af6b19ef0c62477
Alex Felipe <alexfelipevieira@gmail.com>
2018-02-20 10:09:19 -0300

    video 2.2

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
@@ -20,15 +20,18 @@ import static br.com.alura.ceep.ui.activity.NotaActivityConstantes.CODIGO_RESULT
 
 public class FormularioNotaActivity extends AppCompatActivity {
 
+    private int posicaoRecibida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_nota);
 
         Intent dadosRecebidos = getIntent();
-        if(dadosRecebidos.hasExtra(CHAVE_NOTA)){
+        if(dadosRecebidos.hasExtra(CHAVE_NOTA) && dadosRecebidos.hasExtra("posicao")){
             Nota notaRecebida = (Nota) dadosRecebidos
                     .getSerializableExtra(CHAVE_NOTA);
+            posicaoRecibida = dadosRecebidos.getIntExtra("posicao", -1);
             TextView titulo = findViewById(R.id.formulario_nota_titulo);
             titulo.setText(notaRecebida.getTitulo());
             TextView descricao = findViewById(R.id.formulario_nota_descricao);
@@ -55,6 +58,7 @@ public class FormularioNotaActivity extends AppCompatActivity {
     private void retornaNota(Nota nota) {
         Intent resultadoInsercao = new Intent();
         resultadoInsercao.putExtra(CHAVE_NOTA, nota);
+        resultadoInsercao.putExtra("posicao", posicaoRecibida);
         setResult(CODIGO_RESULTADO_NOTA_CRIADA,resultadoInsercao);
     }
 
