a6176d7a574f07c44db670676708e52952460b6d
Alex Felipe <alexfelipevieira@gmail.com>
2021-01-27 09:41:24 -0300

    atualizando aula

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
@@ -2,8 +2,8 @@ package br.com.alura.ceep.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
